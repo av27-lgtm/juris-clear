c206996ae4ceaf59874caf50025dc5fb6e4e056e
av27-lgtm <gharaqeshyanav27@gmail.com>
2026-02-26 22:27:39 +0400

    Update app.py

--- app.py
+++ app.py
@@ -385,7 +385,7 @@ st.markdown("### ⚙️ Параметры анализа")
 c1, c2 = st.columns(2)
 
 with c1:
-    st.markdown("**Ваша роль:**")
+    st.write("**Ваша роль:**")
     user_role = st.pills(
         "Роль", 
         [
